6bd65ed4e41c2137a2305b2f4e9565adb19933c7
Anna Sychugina <anna@emma-app.com>
2022-08-01 13:40:32 +0400

    fix tests

--- jestFileTransformer.js
+++ jestFileTransformer.js
@@ -2,8 +2,6 @@ const path = require('path');
 
 module.exports = {
   process(sourceText, sourcePath, options) {
-    return {
-      code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
-    };
+    return `module.exports = ${JSON.stringify(path.basename(sourcePath))};`;
   },
 };
